d5410f722b8259669a815d49fa750aab058f154c
Temoor Ali <temoor556@gmail.com>
2024-12-11 15:06:22 +0300

    Change runs to 2 hours only not 4

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 while true; do
-	python3 browse_emu_mixed_simplified.py 240 LOW NO 2
-	python3 browse_emu_mixed_simplified.py 240 LOW YES 2
-	python3 browse_emu_mixed_simplified.py 240 MEDIUM YES 2
-	python3 browse_emu_mixed_simplified.py 240 HIGH YES 2
+	python3 browse_emu_mixed_simplified.py 120 LOW NO 2
+	python3 browse_emu_mixed_simplified.py 120 LOW YES 2
+	python3 browse_emu_mixed_simplified.py 120 MEDIUM YES 2
+	python3 browse_emu_mixed_simplified.py 120 HIGH YES 2
 done
