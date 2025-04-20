e88ed67817da065a17f798cdd70b4f412a5dc620
Temoor Ali <temoor556@gmail.com>
2025-04-20 13:53:49 +0300

    Fix extraction from single pcap

--- feature_extraction/pcaps/process_single_pcap.py
+++ feature_extraction/pcaps/process_single_pcap.py
@@ -20,27 +20,25 @@ if not os.path.exists(pcap_path):
 
 # Get orig name of file 
 filename = os.path.basename(pcap_path)[:-5]
-folder_name = f"data/processed/{filename}"
+output_folder = f"data/processed/{filename}"
 
 # base path of scripts
-base_path = "feature_extraction/pcaps"
+extract_feats_folder = "feature_extraction/pcaps"
 
 # Ensure the folder exists
-os.makedirs(folder_name, exist_ok=True)
+os.makedirs(output_folder, exist_ok=True)
 
 # Commands to execute
 commands = [
-    f"cp {pcap_path} {folder_name}/mixed.pcap",
-    f"zeek -C -r {folder_name}/mixed.pcap \"LogAscii::use_json=T\" local",
-    # f"python {base_path}/rename_logs.py --folder {folder_name}",
-    f"python {base_path}/extract_domains.py --folder {folder_name}",
-    f"python {base_path}/split_connections.py --folder {folder_name}",
-    f"sh {base_path}/tshark.sh {pcap_path} {folder_name}",
-    f"python {base_path}/get_labeled_conn.py --folder_path {folder_name}"
-    f"rm {folder_name}/mixed.pcap"
+    f"cp {pcap_path} {output_folder}/mixed.pcap",
+    f"cd {output_folder} && zeek -C -r mixed.pcap 'LogAscii::use_json=T' local",
+    f"python3 {os.path.join(extract_feats_folder, 'extract_domains.py')} --folder {output_folder}",
+    f"python3 {os.path.join(extract_feats_folder, 'split_connections.py')} --folder {output_folder}",
+    f"sh {os.path.join(extract_feats_folder, 'tshark.sh')} {pcap_path} {output_folder}", 
+    f"python3 {os.path.join(extract_feats_folder, 'get_labeled_conn.py')} --folder_path {output_folder}",
+    f"rm {output_folder}/mixed.pcap",
 ]
 
-# Execute commands in sequence
 for command in commands:
     try:
         print(f"Running: {command}")
